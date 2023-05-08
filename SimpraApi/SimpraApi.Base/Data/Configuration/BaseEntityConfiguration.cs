0d3c575bbea248a2718ddbdad04bc8e8ece4d7a3
Furkan <sahinf5488@gmail.com>
2023-05-08 15:39:40 +0300

    base entity configuration implemented

---
+++ SimpraApi/SimpraApi.Base/Data/Configuration/BaseEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SimpraApi.Base;
+public class BaseEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        builder.Property(x => x.Status).IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+    }
+}
